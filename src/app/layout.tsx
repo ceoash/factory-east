6d196b83bc42a81397ba7a66576a7acdd30ec9ad
ceoash <ceoash@outlook.com>
2025-03-28 17:13:24 +0000

    metadata update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased !bg-white`}
       >
-
         {children}
         <Footer />
       </body>
